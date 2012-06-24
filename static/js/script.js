7ccb426967a5702e9fef4600418fb491bc386093
John Schimmel <john.schimmel@gmail.com>
2012-06-24 11:44:34 -0400

    new styles, survey choice buttons not working

--- static/js/script.js
+++ static/js/script.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
     // iOS label click event fix
     // via http://v4.thewatchmakerproject.com/blog/how-to-fix-the-broken-ipad-form-label-click-issue/
     if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
-		jQuery('label[for]').click(function () {
+		jQuery('label[for]').live('click',function () {
 			var el = jQuery(this).attr('for');
 			if (jQuery('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !jQuery('#' + el).attr('selected'))) {
 				return;
